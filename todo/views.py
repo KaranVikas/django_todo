ab4743cae475095a7660b1f29e1814f3d1f9672b
KaranVikas <karanvikas15@gmail.com>
2025-09-05 14:58:53 -0400

    Implement task management features: add edit, delete, mark as done/undone functionalities and corresponding templates

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 
 from todo.models import Task
@@ -7,3 +7,38 @@ def addTask(request):
   task = request.POST['task']
   Task.objects.create(task=task)
   return redirect('home')
+
+def mark_as_done(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  task.is_completed = True
+  task.save()
+  return redirect('home')
+
+def mark_as_undone(request, pk):
+  task = get_object_or_404(Task,pk=pk)
+  print(task)
+  task.is_completed = False
+  task.save()
+  return redirect('home')
+
+def edit_task(request, pk):
+  get_task = get_object_or_404(Task, pk=pk)
+  if request.method == 'POST':
+    new_task = request.POST['task']
+    get_task.task = new_task
+    get_task.save()
+    print(new_task)
+    print(get_task)
+    return redirect('home')
+
+  else:
+    context = {
+      'get_task': get_task,
+    }
+    return render(request, 'edit_task.html', context)
+  
+def delete_task(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  task.delete()
+  return redirect('home')
+  
